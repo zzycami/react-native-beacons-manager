07b2022a7bca9c809a2801f8b2b89d1c4bc4545e
周泽勇 <zzycami@foxmail.com>
2017-06-27 15:31:47 +0800

    web版本修改

--- index.web.js
+++ index.web.js
@@ -2,20 +2,69 @@
 
 import { Platform } from 'react-native';
 
-export default class RNiBeacons {
-  constructor() {}
-
-  requestAlwaysAuthorization() {}
-
-  requestWhenInUseAuthorization() {}
-
-  getAuthorizationStatus() {}
-
-  startMonitoringForRegion(region) {}
-
-  startRangingBeaconsInRegion(region) {}
-
-  stopMonitoringForRegion(region) {}
-
-  stopRangingBeaconsInRegion(region: BeaconRegion) {}
+function requestAlwaysAuthorization() {
 }
+
+/**
+ * request when app in use authorization (bare minimum for beacons)
+ * IMPORTANT: To be effective your info.plist file should have 'Privacy - Location When In Use Usage Description' key defined (hopefully 'react-native init' should have set it for you)
+ */
+function requestWhenInUseAuthorization() {
+}
+
+/**
+ * get authorization status
+ *
+ * @returns {() => AuthorizationStatus} instant callback (not async)
+ */
+function getAuthorizationStatus() {
+}
+
+/**
+ * start monitoring for a region
+ *
+ * @param {BeaconRegion} region region to monitor (identifier + uuid -> major and minor are optional)
+ */
+function startMonitoringForRegion(region) {
+}
+
+/**
+* start ranging for a region
+*
+* @param {BeaconRegion} region region to scan (identifier + uuid -> major and minor are optional)
+*/
+function startRangingBeaconsInRegion(region){
+}
+
+/**
+* stop monitoring for a region
+*
+* @param {BeaconRegion} region region (identifier + uuid -> major and minor are optional)
+*/
+function stopMonitoringForRegion(region){
+}
+
+/**
+* stop ranging for a region
+*
+* @param {BeaconRegion} region region (identifier + uuid -> major and minor are optional)
+*/
+function stopRangingBeaconsInRegion(region) {
+}
+
+function startUpdatingLocation() {}
+
+function stopUpdatingLocation(){}
+
+
+export default {
+  requestAlwaysAuthorization,
+  requestWhenInUseAuthorization,
+  getAuthorizationStatus,
+  startMonitoringForRegion,
+  startRangingBeaconsInRegion,
+  stopMonitoringForRegion,
+  stopRangingBeaconsInRegion,
+  startUpdatingLocation,
+  stopUpdatingLocation
+};
